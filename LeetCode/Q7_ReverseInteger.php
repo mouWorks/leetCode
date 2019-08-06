14441e427f337bf859e304adc8d38a763f826bb1
Mou <mou.wang@g.ncu.edu.tw>
2019-08-06 17:39:09 +0800

    Get 1 done with Crappy answer

---
+++ LeetCode/Q7_ReverseInteger.php
@@ -0,0 +1,80 @@
+<?php
+namespace LeetCode\Q7;
+use PHPUnit\Framework\TestCase;
+
+class Q7_ReverseInteger extends TestCase
+{
+    /**
+     * @var Solution
+     */
+    private $solution;
+
+    public function setUp()
+    {
+        $this->solution = new Solution();
+    }
+
+    public function test_1_正數()
+    {
+        $int = 123;
+        $response = $this->solution->reverseInt($int);
+        $this->assertEquals(321, $response);
+    }
+
+    public function test_2_負數()
+    {
+        $int = -123;
+        $response = $this->solution->reverseInt($int);
+        $this->assertEquals(-321, $response);
+    }
+
+    public function test_3_120()
+    {
+        $int = 120;
+        $response = $this->solution->reverseInt($int);
+        $this->assertEquals(21, $response);
+    }
+
+    public function test_4_1534236469 ()
+    {
+        $int = 1534236469;
+        $response = $this->solution->reverseInt($int);
+        $this->assertEquals(0, $response);
+    }
+
+}
+
+class Solution
+{
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    public function reverseInt($n)
+    {
+
+        $boolFlip = $n > 0 ? false : true;
+
+        if($boolFlip){
+            $n = $n* -1;
+        }
+
+        $reverse = 0;
+        while ($n > 0)
+        {
+            $reverse = $reverse * 10;
+            $reverse = $reverse + $n % 10;
+            $n = (int)($n/10);
+        }
+
+        if($reverse > 2147483647){
+            return 0;
+        }
+
+        if($boolFlip){
+            return $reverse * -1;
+        }
+
+        return $reverse;
+    }
+}
