3473b18a1e26a23346ce3354e99e25fb7533808e
mouWorks <mou.wang@g.ncu.edu.tw>
2019-09-16 19:29:52 +0800

    Update

--- LeetCode/Q136_SingleNumberTest.php
+++ LeetCode/Q136_SingleNumberTest.php
@@ -9,7 +9,7 @@ use PHPUnit\Framework\TestCase;
  * @package LeetCode\Q1
  *
  */
-class Q1_TwoSumTest extends TestCase
+class Q136_SingleNumberTest extends TestCase
 {
     /**
      * @var Solution
@@ -21,12 +21,12 @@ class Q1_TwoSumTest extends TestCase
         $this->solution = new Solution();
     }
 
-    public function test1_Sample1()
-    {
-        $nums = [2,2,1];
-        $result = $this->solution->single($nums);
-        $this->assertEquals(1, $result);
-    }
+//    public function test1_Sample1()
+//    {
+//        $nums = [2,2,1];
+//        $result = $this->solution->single($nums);
+//        $this->assertEquals(1, $result);
+//    }
 
     public function test2_Sample2()
     {
@@ -35,6 +35,12 @@ class Q1_TwoSumTest extends TestCase
         $this->assertEquals(4, $result);
     }
 
+    public function test3_Sample3()
+    {
+        $nums = [1,3,1,-1,3];
+        $result = $this->solution->single($nums);
+        $this->assertEquals(-1, $result);
+    }
 }
 
 class Solution
@@ -45,6 +51,20 @@ class Solution
      */
     public function single($nums)
     {
-        return 1;
+        $size = count($nums);
+
+        for($i=0;$i<$size;$i++){
+            for($j=$i+1;$j<$size;$j++){
+                echo 'i:' . $i . ',j:' . $j . '|';
+                if($nums[$i] == $nums[$j]){
+                   unset($nums[$i]);
+                   unset($nums[$j]);
+                   $size -= 2;
+                   //$nums = array_values($nums);
+                }
+            }
+        }
+        $result = array_values($nums);
+        return $result[0];
     }
 }
